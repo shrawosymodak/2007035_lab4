384aa6d8847115e5f746b369fdb35ddf5e215793
shrawosy <modak2007035@stud.kuet.ac.bd>
2024-02-08 09:39:21 +0600

    swe labtask

--- Main.java
+++ Main.java
@@ -1,6 +1,4 @@
-// 2007035
-//i have completed this code using bridge design pattern
-interface TV {
+    interface TV { // bridg and factory.roll 2007035
     boolean isEnabled();
     void enable();
     void disable();
@@ -14,32 +12,22 @@ class SmartTV implements TV {
     private boolean powerOn = false;
     private int volume = 50;
     private int channel = 10;
-    public boolean isEnabled() {
-        return powerOn;
-    }
-    public void enable() {
-        powerOn = true;
-    }
-    public void disable() {
-        powerOn = false;
-    }
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-    public int getVolume() {
-        return volume;}
-           
-    public void setChannel(int channel) {
-        this.channel = channel;
-    }
-    
-    public int getChannel() {
-        return channel;
-    }
 
-    public void Youtube() {
-        System.out.println(" Showing Youtube");
-    }
+    public boolean isEnabled() { return powerOn; }
+
+    public void enable() { powerOn = true; }
+
+     public void disable() { powerOn = false; }
+
+    public int getVolume() { return volume; }
+
+    public void setVolume(int volume) { this.volume = volume; }
+
+    public int getChannel() { return channel; }
+
+    public void setChannel(int channel) { this.channel = channel; }
+
+    public void Youtube() { System.out.println("SmartTV: Showing Youtube"); }
 }
 
 class GeneralTV implements TV {
@@ -47,39 +35,36 @@ class GeneralTV implements TV {
     private int volume = 10;
     private int channel = 2;
 
-    
-    public boolean isEnabled() {
-        return powerOn;
-    }
-    public void enable() {
-        powerOn = true;
-     }
-    public void disable() {
-        powerOn = false;
- }
-   public void setVolume(int volume) {
-    this.volume = volume;
-}
+    public boolean isEnabled() { return powerOn; }
 
-    public int getVolume() {
-        return volume;
-    }
-    public void setChannel(int channel) {
-        this.channel = channel;
-    }
-   
-    public int getChannel() {
-        return channel;
-    }
-   
+    public void enable() { powerOn = true; }
+
+    public void disable() { powerOn = false; }
+
+    public int getVolume() { return volume; }
+
+    public void setVolume(int volume) { this.volume = volume; }
+
+    public int getChannel() { return channel; }
+
+    public void setChannel(int channel) { this.channel = channel; }
 }
 
 abstract class Remote {
     protected TV tv;
 
-    Remote(TV tv) {
-        this.tv = tv;
-    }
+    Remote(TV tv) { this.tv = tv; }
+
+    public abstract void togglePower();
+     public abstract void volumeUp();
+    public abstract void volumeDown();
+ public abstract void channelUp();
+    public abstract void channelDown();
+}
+
+class GeneralRemote extends Remote {
+
+    GeneralRemote(TV tv) { super(tv); }
 
     public void togglePower() {
         if (tv.isEnabled()) {
@@ -87,14 +72,68 @@ abstract class Remote {
             System.out.println("Power turned off");
         } else {
             tv.enable();
-            System.out.println("Powerturned on");
+            System.out.println("Power turned on");
         }
     }
 
     public void volumeUp() {
         if (tv.isEnabled()) {
             tv.setVolume(tv.getVolume() + 10);
-            System.out.println("Volume increasedby 10");
+            System.out.println("Volume increased by 10");
+        } else {
+            System.out.println("Please turn on the TV first");
+        }
+    }
+
+    public void volumeDown() 
+    {
+        if (tv.isEnabled())
+         {
+            tv.setVolume(tv.getVolume() - 10);
+            System.out.println("Volume decreased by 10");
+        } else {
+     System.out.println("Please turn on the TV first");
+        }
+    }
+
+    public void channelUp()
+     {
+        if (tv.isEnabled()) {
+            tv.setChannel(tv.getChannel() + 5);
+            System.out.println("Channel increased by 5");
+        } else {
+            System.out.println("Please turn on the TV first");
+        }
+    }
+
+    public void channelDown() {
+        if (tv.isEnabled()) {
+     tv.setChannel(tv.getChannel() - 5);
+            System.out.println("Channel decreased by 5");
+        } else {
+            System.out.println("Please turn on the TV first");
+        }
+    }
+}
+
+class SmartRemote extends Remote {
+
+    SmartRemote(TV tv) { super(tv); }
+
+    public void togglePower() {
+          if (tv.isEnabled()) {
+        tv.disable();
+            System.out.println("Power turned off");
+        } else {
+            tv.enable();
+            System.out.println("Power turned on");
+        }
+    }
+
+    public void volumeUp() {
+if (tv.isEnabled()) {
+            tv.setVolume(tv.getVolume() + 10);
+            System.out.println("Volume increased by 10");
         } else {
             System.out.println("Please turn on the TV first");
         }
@@ -104,7 +143,7 @@ abstract class Remote {
         if (tv.isEnabled()) {
             tv.setVolume(tv.getVolume() - 10);
             System.out.println("Volume decreased by 10");
-        } else {
+        else {
             System.out.println("Please turn on the TV first");
         }
     }
@@ -112,7 +151,7 @@ abstract class Remote {
     public void channelUp() {
         if (tv.isEnabled()) {
             tv.setChannel(tv.getChannel() + 5);
-            System.out.println("Channel increasedby 5");
+            System.out.println("Channel increased by 5");
         } else {
             System.out.println("Please turn on the TV first");
         }
@@ -126,39 +165,37 @@ abstract class Remote {
             System.out.println("Please turn on the TV first");
         }
     }
+
+void showYoutube() { ((SmartTV) tv).Youtube(); }
 }
 
-class SmartRemote extends Remote {
+class TVFactory {
+    private static final TV smartTV = new SmartTV();
+    private static final TV generalTV = new GeneralTV();
 
-    SmartRemote(TV tv) {
-        super(tv);
-    }
+    public static TV getSmartTV() { return smartTV; }
 
-    void showYoutube() {
-        ((SmartTV) tv).Youtube();
-    }
+    public static TV getGeneralTV() { return generalTV; }
 }
 
 public class Main {
     public static void main(String[] args) {
 
-        GeneralTV generaltv = new GeneralTV();
-SmartTV smarttv = new SmartTV();
+        TV generaltv = TVFactory.getGeneralTV();
+        TV smarttv = TVFactory.getSmartTV();
 
-        Remote remote = new Remote(generaltv);
-        remote.togglePower();
-        remote.volumeUp();
-        remote.channelUp();
-         remote.channelDown();
-        remote.volumeDown();
-
-         SmartRemote smartremote = new SmartRemote(smarttv);
-        smartremote.togglePower();
-        smartremote.volumeUp();
-        smartremote.channelUp();
-        smartremote.channelDown();
-        smartremote.volumeDown();
-        smartremote.showYoutube();
-       
+        Remote generalRemote = new GeneralRemote(generaltv);
+        generalRemote.togglePower();
+        generalRemote.volumeUp();
+        generalRemote.channelUp();
+          generalRemote.channelDown();
+            generalRemote.volumeDown();
+        SmartRemote smartRemote = new SmartRemote(smarttv);
+        smartRemote.togglePower();
+        smartRemote.volumeUp();
+        smartRemote.channelUp();
+          smartRemote.channelDown();
+        smartRemote.volumeDown();
+        smartRemote.showYoutube();
     }
 }
